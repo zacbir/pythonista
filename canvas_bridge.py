53466d93b772a3bc87c86f5a9dfbcc7bc5193ec4
Zachery Bir <zbir@urbanape.com>
2014-01-16 13:47:13 -0500

    Add move_to

--- canvas_bridge.py
+++ canvas_bridge.py
@@ -13,6 +13,8 @@ except ImportError:
     fill_ellipse = oval
     set_stroke_color = stroke
     set_line_width = strokewidth
+    move_to = moveto
+    
     
     def draw_rect(x, y, width, height):
         """capture and reset current fill color"""
@@ -30,10 +32,3 @@ except ImportError:
 
     set_size = size
 
-set_size(1024.0, 1024.0)
-set_fill_color(1.0, 0.0, 0.0, 1.0)
-set_stroke_color(1.0, 0.0, 0.0, 1.0)
-draw_rect(100.0, 100.0, 100.0, 100.0)
-fill_rect(300.0, 100.0, 100.0, 100.0)
-draw_ellipse(100.0, 300.0, 100.0, 100.0)
-fill_ellipse(300.0, 300.0, 100.0, 100.0)
